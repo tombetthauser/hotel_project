660ea508e399381ad7cf62ed17e5d4ba56187ba3
tombetthauser <tombetthauser@gmail.com>
2019-10-25 19:29:36 -0700

    Completed hotel project

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,5 +1,44 @@
+require "byebug"
 require_relative "room"
 
 class Hotel
+
+  attr_accessor :rooms
   
+  def initialize(name, input_rooms)
+    @name = name
+    @rooms = {}; input_rooms.each { |k,v| @rooms[k] = Room.new(v) }
+  end
+
+  def name
+    @name.split(" ").map(&:capitalize).join(" ")
+  end
+
+  def room_exists?(room_name)
+    self.rooms[room_name] ? true : false
+  end
+
+  def check_in(person, room_name)
+    if room_exists?(room_name)
+      success = self.rooms[room_name].add_occupant(person)
+      if success 
+        p "check in successful"
+      else
+        p "sorry, room is full"
+      end
+    else
+      p "sorry, room does not exist"
+    end
+  end
+
+  def has_vacancy?
+    rooms.values.any? { |room| room.occupants.count < room.capacity }
+  end
+
+  def list_rooms
+    self.rooms.each do |name, room| 
+      puts "#{name} : #{room.available_space}"
+    end
+  end
+
 end
--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,31 @@
+require "byebug"
+
 class Room
 
+  attr_reader :capacity, :occupants
+
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def full?
+    @occupants.count < @capacity ? false : true
+  end
+
+  def available_space
+    @capacity - @occupants.count
+  end
+
+  def add_occupant(name)
+    return false if full?
+    @occupants << name
+    true
+  end
+
 end
+
+
+# started Room class / Part 01 ~1:25p
+# finished Room class / Part 01 ~1:32p
+# 7min(with ternary bug)
